2de9ac3c36c4c04e3a4e8623fe1c69d6341154a2
ryan pujo <pujotriantoro@gmail.com>
2023-09-02 16:29:30 +0700

    notif icon: added

---
+++ src/routes/Notification.svelte
@@ -0,0 +1,9 @@
+<script>
+	import MdNotifications from 'svelte-icons/md/MdNotifications.svelte'
+
+</script>
+
+<div class="w-8 h-8 btn btn-ghost btn-circle mr-4 indicator">
+  <span class="indicator-item mt-2 w-8 badge badge-info">99+</span>
+  <MdNotifications />
+</div>
