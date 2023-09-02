5f966b139039d67eb5a7f09d35c47353c9883def
ryan pujo <pujotriantoro@gmail.com>
2023-09-02 16:56:07 +0700

    moving getProductsByCategory call to onMount

--- src/routes/Carousel.svelte
+++ src/routes/Carousel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import type { PageData } from "./$types";
 	import type { Product } from "./+page";
 	import ProductCard from "./ProductCard.svelte";
@@ -12,7 +13,10 @@
     const json: Product[] = (await res.json()).products;
     return json
   }
-  const promise = getProductsByCategory();
+  let promise: Promise<Product[]> | null;
+  onMount(() => {
+    promise = getProductsByCategory();
+  });
 </script>
 
 <div class="mx-10 mb-10">
@@ -22,6 +26,7 @@
     <a class="btn btn-primary btn-xs rounded-md" {href}>View all</a>
   </div>
   <div class="carousel w-96 md:w-auto md:flex md:justify-between">
+    {#if promise}
     {#await promise}
       <div class="flex justify-center w-full">
         <span class="loading loading-spinner loading-md"></span>
@@ -31,5 +36,6 @@
         <ProductCard class="w-1/2 md:w-96 carousel-item bg-base-100 shadow-xl mr-5" {product} />
       {/each}
     {/await}
+    {/if}
   </div>
 </div>
