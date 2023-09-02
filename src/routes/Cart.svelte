a90f699792d5786026ae262632fb7d307e3c6c9c
ryan pujo <pujotriantoro@gmail.com>
2023-09-02 16:28:46 +0700

    cart icon: added

---
+++ src/routes/Cart.svelte
@@ -0,0 +1,9 @@
+<script>
+	import MdAddShoppingCart from 'svelte-icons/md/MdAddShoppingCart.svelte';
+  let clazz = "";
+  export {clazz as class};
+</script>
+
+<div class={"w-8 h-8 mr-4 btn btn-ghost btn-square " + clazz}>
+  <MdAddShoppingCart />
+</div>
