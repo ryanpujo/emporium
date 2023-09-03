6032c52b5430580c302c2d8aeb9de20627faa03c
ryan pujo <pujotriantoro@gmail.com>
2023-09-03 15:06:58 +0700

    Carousel Component: added

---
+++ src/lib/components/carousel/Carousel.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Item from "./Item.svelte";
+
+let clazz = "";
+export {clazz as class};
+</script>
+
+<div class={"carousel carousel-center rounded-box w-full md:w-auto " + clazz}>
+  <slot {Item} />
+</div>
